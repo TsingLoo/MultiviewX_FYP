990c25a0f07d9d52cafaa18f19091dc00c50925b
TsingLoo <Tsingloo@outlook.com>
2023-07-17 01:45:36 +0800

    Visualize FOV

--- generateView.py
+++ generateView.py
@@ -13,7 +13,7 @@ from math import sqrt
 from unitConversion import *
 from datasetParameters import *
 
-
+VIEW_DISTANCE = 8
 AOICorners2D = [[0, 0],
 
             [0 + MAP_WIDTH, 0], 
@@ -92,12 +92,12 @@ def generate_View():
         print()
         
 
-    json_file = 'viewarea.json'
+    #json_file = 'viewarea.json'
 
     # Save the dictionary to a JSON file
 
-    with open(json_file, 'w') as file:
-        json.dump(json_dict, file)
+    #with open(json_file, 'w') as file:
+        #json.dump(json_dict, file)
                
 
     
@@ -120,69 +120,48 @@ def calculate_angle(p1, p2):
 
 def draw_views():
     fig, ax = plt.subplots()
-    json_file = 'viewarea.json'
-    
-    # Load the dictionary from the JSON file
-    with open(json_file, 'r') as file:
-        json_dict = json.load(file)
 
-    # Define the AOICorners2D
-    AOICorners2D = json_dict['AOICorners2D']
-    AOICorners2D.append(AOICorners2D[0]) # repeat the first point to create a 'closed loop'
-    AOI_polygon = Polygon(AOICorners2D)
+    AOICorners2D = [[0, 0], [0 + MAP_WIDTH, 0], [0 + MAP_WIDTH, 0 + MAP_HEIGHT], [0, 0 + MAP_HEIGHT]]
+    AOICorners2D.append(AOICorners2D[0]) 
+    xs, ys = zip(*AOICorners2D)
+    plt.plot(xs, ys, 'r-')
 
-    # Draw the AOICorners2D
-    xs, ys = zip(*AOICorners2D) # create lists of x and y values
-    plt.plot(xs, ys, 'r-', label='AOI') # 'r-' is the color red
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    for idx in range(NUM_CAM):
+        rvec, tvec, cameraMatrix, _ = get_calibration(idx)
+        camera_position = get_camera_position(rvec, tvec)
+        camera_position = camera_position[:2]
+        fov_x = 2 * np.arctan(IMAGE_WIDTH / (2 * cameraMatrix[0, 0]))
 
-    # Prepare a colormap
-    cmap = plt.get_cmap('tab10')
+        # Calculate rotation matrix and find pan angle
+        R, _ = cv2.Rodrigues(rvec)
+        pan_angle = np.arctan2(R[1, 0], R[0, 0]) + np.pi/2  # adjust angle by subtracting 90 degrees
 
-    # Draw the polygons from viewarea_dict
-    for idx, (key, cam_dict) in enumerate(json_dict.items()):
-        if key == 'AOICorners2D': continue 
+        # Calculate points on the edge of the FOV
+        fov_angles = np.linspace(-fov_x / 2, fov_x / 2, 30)
+        fov_points = VIEW_DISTANCE * np.array([np.cos(fov_angles + pan_angle), np.sin(fov_angles + pan_angle)]).T + camera_position
 
-        points = cam_dict['viewarea_corners']
-        points.append(points[0])
+        # Append camera position as the first and last points
+        fov_points = np.concatenate(([camera_position], fov_points, [camera_position]), axis=0)
+        
+        xs, ys = zip(*fov_points)
+        plt.fill(xs, ys, color=colors[idx % len(colors)], alpha=0.3)  # Fill FOV area with color
 
-        camera_position = cam_dict['camera_position']
-        plt.scatter(camera_position[0], camera_position[1], c=cmap(idx % cmap.N), marker='x')
-        plt.text(camera_position[0], camera_position[1], key, color=cmap(idx % cmap.N))
-
-        # Define the polygons
-        points = ccw_sort(points)
-        camera_polygon = Polygon(points)
-
-        # Check if the polygons intersect and draw the intersection
-        if camera_polygon.intersects(AOI_polygon):
-            intersection = camera_polygon.intersection(AOI_polygon)
-            xs, ys = intersection.exterior.xy
-            plt.fill(xs, ys, alpha=0.5, fc=cmap(idx % cmap.N), label=f'Intersection {key}')
-
-            # Create a list of points with their corresponding angles
-            points_with_angles = [(point, calculate_angle(camera_position, point)) for point in intersection.exterior.coords[:-1]]
-            points_with_angles.sort(key=lambda x: x[1])  # sort by angle
-
-            # Draw lines from the camera to the boundaries of the intersection (FOV lines)
-            for point, _ in [points_with_angles[0], points_with_angles[-1]]:  # get the points with the smallest and largest angle
-                plt.plot([camera_position[0], point[0]], [camera_position[1], point[1]], color=cmap(idx % cmap.N), linestyle='--')
+        plt.scatter(*camera_position, c=colors[idx % len(colors)], marker='x')
+        plt.text(*camera_position, 'cam' + str(idx), color=colors[idx % len(colors)])
 
     ax.set_aspect('equal', adjustable='box')
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Overlap between AOI and Viewarea of cams')
-    ax.set_xticks(np.arange(0, np.ceil(ax.get_xlim()[1]), step=1))  # Setting grid
+    plt.title('AOI and FOVs of cameras')
+    ax.set_xticks(np.arange(0, np.ceil(ax.get_xlim()[1]), step=1))
     ax.set_yticks(np.arange(0, np.ceil(ax.get_ylim()[1]), step=1))
     plt.grid(True)
-    plt.legend()
 
-    # Save the plot as SVG
-    plt.savefig('overlap_viewarea.svg', format='svg')  # save as SVG file
-    shutil.move(json_file, datasetParameters.DATASET_NAME)
+    plt.savefig('FOVs.svg')
 
 
 
-    
 
 if __name__ == '__main__':
     a()
